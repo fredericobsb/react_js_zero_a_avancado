4f676e6d167104aae0b8cf54f01c77e048082b58
frederico <fredyramos0707@gmail.com>
2024-01-27 19:14:24 -0300

    Aula 54 - trabalhando com rotas

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import {Link} from 'react-router-dom';
+
+function Home(){
+    return(
+      <div>
+        <h1>Bem-vindo a pagina Home!</h1>
+        <span>Sujeito Programador.</span>
+        <br/>
+
+        <Link to="/sobre">Sobre</Link>
+      </div>
+    );
+  }
+  
+  export default Home;
